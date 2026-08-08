c881f6d172e68cb035da2c20cd10b28f7112af49
SALMAN KHAN <139510744+SALMAN-ORAKXI@users.noreply.github.com>
2026-08-08 18:28:27 +0500

    final apk

---
+++ .netlify/functions-internal/server/_tanstack-start-manifest_v-CILnxrjF.mjs
@@ -0,0 +1,20 @@
+//#region node_modules/.nitro/vite/services/ssr/assets/_tanstack-start-manifest_v-CILnxrjF.js
+var tsrStartManifest = () => ({ routes: {
+	__root__: {
+		filePath: "E:/moviedock website/moviedock_website/src/routes/__root.tsx",
+		children: ["/", "/sitemap.xml"],
+		preloads: ["/assets/index-CufvQLPj.js"],
+		scripts: [{ attrs: {
+			type: "module",
+			async: !0,
+			src: "/assets/index-CufvQLPj.js"
+		} }]
+	},
+	"/": {
+		filePath: "E:/moviedock website/moviedock_website/src/routes/index.tsx",
+		children: void 0,
+		preloads: ["/assets/routes-BhgUAb0f.js"]
+	}
+} });
+//#endregion
+export { tsrStartManifest };
