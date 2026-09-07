37cddf49f170c9f15409bf057da2e700c667ed1b
SALMAN KHAN <139510744+SALMAN-ORAKXI@users.noreply.github.com>
2026-09-07 07:21:29 +0500

    MovieDock v1.2.1 Production Release with GitHub Actionsss

--- src/App.tsx
+++ src/App.tsx
@@ -128,7 +128,7 @@ export default function App() {
       let loaded = 0;
 
       const reader = response.body?.getReader();
-      const chunks: Uint8Array[] = [];
+      const chunks: BlobPart[] = [];
 
       if (reader) {
         while (true) {
